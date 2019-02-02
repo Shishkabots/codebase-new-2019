58a7b79851d1129ed09058f0699ce61c1bd4fe13
William Pan <william95135@gmail.com>
2019-02-02 15:03:47 -0800

    need commandgroup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -234,7 +234,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    new DriveTrainControl().start();
+    new TeleOpCommands().start();
   }
 
   /**
---
+++ src/main/java/frc/robot/commands/TeleOpCommands.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class TeleOpCommands extends CommandGroup {
+
+    public static DriveTrainControl drive;
+    public TeleOpCommands() {
+        addParallel(new DriveTrainControl());
+       //addParallel(new LinearSlideControl());
+       //addParallel(new IntakeControl());
+       //addParallel(new ClawsControl());
+       //addParallel(new PlatformControl());
+       //addParallel(new GripperControl());
+    }
+    public void end() {
+        drive.end();
+    }
+}
