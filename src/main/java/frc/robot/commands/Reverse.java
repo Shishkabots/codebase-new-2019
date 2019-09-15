dcd24422623535684eb1a399b98b098f08b5094f
Isita Talukdar <isitatalukdar@gmail.com>
2019-09-15 14:45:26 -0700

    Cleaned Unused Codea

--- src/main/java/frc/robot/commands/Reverse.java
+++ src/main/java/frc/robot/commands/Reverse.java
@@ -19,7 +19,6 @@ public class Reverse extends Command {
     @Override
     protected void initialize() {
         //SmartDashboard.putNumber("Interrupt: ", 0);
-        //Robot.m_drivetrain.move(0, 0);
     }
         
     // Called repeatedly when this Command is scheduled to run
@@ -27,18 +26,7 @@ public class Reverse extends Command {
     @Override
     protected void execute() {
         Robot.m_drivetrain.reverse = !Robot.m_drivetrain.reverse;
-        // double speed = Robot.m_oi.boost.get() ? 1.0 : .5;
-        // double lTrigger = Robot.m_oi.controllerOne.getRawAxis(3);
-        // double rTrigger = Robot.m_oi.controllerOne.getRawAxis(2);
-        // double turnAxis = Robot.m_oi.controllerOne.getRawAxis(0);
-        
 
-        
-        // //3 is right trigger, 2 is left trigger, 0 is x axis of left stick, unsure of math
-    	// Robot.m_drivetrain.moveWithCurve(
-        // (rTrigger - lTrigger) * speed,
-        // turnAxis * 0.5 * (rTrigger > lTrigger ? 1 : -1),
-        // true);
         
             
         // //SmartDashboard.putNumber("Encoder 1: ", e1.getDistance());
